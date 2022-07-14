f56ef526cc8188d64d4d2f9169642b9c72baca10
Sima <tarverdans15@gmail.com>
2022-07-14 22:20:16 +0300

    git commit -am

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,45 @@ let canvas = document.querySelector("#game-canvas")
 let context = canvas.getContext("2d")
 scoreBlock = document.querySelector(".game-score .score-count")
 drawScore();
+function gameLoop(){
+    requestAnimationFrame(gameLoop);
+    if (++mysteps.step < mysteps.maxStep){
+        return;
+    }
+    mysteps.step = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    drawBarry();
+    drawSnake();
+}
+function drawSnake(){ // сама змейка
+    snake.x+=snake.dx;
+    snake.y+= snake.dy;
+    snake.tails.unshift({x: snake.x, y: snake.y})
+    if(snake.tails.length > snake.maxTails){
+        snake.tails.pop();
+    }
+    snake.tails.forEach(function(el,index){
+        if (index == 0){
+            context.fillStyle = "#FA0556"
+        } else{
+            context.fillStyle = "#A00034";
+        }
+        context.fillRect(el.x, el.y, mysteps.sizeCell, mysteps.sizeCell);
+        if (el.x === berry.x && el.y === berry.y){
+            snake.maxTails++;
+            incScore()
+            randomPositionBerry();
+        }
+        for (let i = index + 1; i<snake.tails.length; i++){
+            if (el.x == snake.tails[i].x && el.y == snake.tails[i].y){
+                refresGame();
+            }
+        }
+    }
+    )
+}
+function refresGame(){}
+function randomPositionBerry(){}
 function incScore(){ // обработки очков, увеличивает число на один
     score++;
     drawScore();
